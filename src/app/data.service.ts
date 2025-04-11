9f6cf25f801208ecc6b1c9f6f35ec664935dae22
Zach-Adolphsen <zladolphsen@gmail.com>
2025-04-11 10:55:32 -0500

    added HTTP functions

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,17 +1,35 @@
-import { Injectable } from '@angular/core';
-import { posts } from './post-data';
+import { inject, Injectable } from '@angular/core';
 import { Post } from './post';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  private http = inject(HttpClient);
+  url = 'https://jsonplaceholder.typicode.com/posts';
 
-  posts = posts;
+  getPosts(): Observable<Post[]> {
+    // const params = new HttpParams().set('_limit', 20);
+    return this.http.get<Post[]>(this.url);
+  }
 
-  constructor() { }
+  getPostById(id: number): Observable<Post> {
+    // const params = new HttpParams().set('id', id);
+    // return this.http.get<Post[]>(this.url, { params });
 
-  getPostById(id:number): Post{
-    return this.posts.filter(post => post.id == id)[0];
+    return this.http.get<Post>(`&{this.url}/${id}`);
+  }
+
+  addPost() {
+    const post = {
+      id: 101,
+      userId: 1000,
+      title: 'dolerum dolerum',
+      body: 'adadsofijeoi',
+    };
+
+    return this.http.post(this.url, post);
   }
 }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -2,21 +2,31 @@ import { Component, inject } from '@angular/core';
 import { DataService } from '../data.service';
 import { Post } from '../post';
 import { RouterModule } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   imports: [RouterModule],
   templateUrl: './post-list.component.html',
-  styleUrl: './post-list.component.css'
+  styleUrl: './post-list.component.css',
 })
 export class PostListComponent {
-
   postService = inject(DataService);
   posts: Post[] = [];
 
-  ngOnInit(){
-    this.posts = this.postService.posts;
+  ngOnInit() {
+    // this.postService.getPosts().subscribe((data) => (this.posts = data));
+    this.postService
+      .getPosts()
+      .pipe(
+        map((posts: Post[]) =>
+          posts.filter((post) => post.title.toLowerCase().includes('dolorem'))
+        )
+      )
+      .subscribe((data) => (this.posts = data));
   }
-  
 
+  addPost() {
+    this.postService.addPost().subscribe((data) => console.log(data));
+  }
 }
